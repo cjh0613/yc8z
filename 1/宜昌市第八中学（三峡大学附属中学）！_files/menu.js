77d528e82d3383f22f02226b67e3d3c4e893c042
cjh0613 <cjhbruce@foxmail.com>
2020-03-14 16:42:19 +0800

    .

---
+++ 1/宜昌市第八中学（三峡大学附属中学）！_files/menu.js
@@ -0,0 +1,130 @@
+stm_bm(["menu1a91",830,"","images/blank.gif",0,"","",0,0,250,0,1000,1,0,0,"","",0,0,1,2,"default","hand",""],this);
+stm_bp("p0",[0,4,0,0,0,0,5,0,100,"",-2,"",-2,50,0,0,"#999999","transparent","",3,0,0,"#000000"]);
+stm_ai("p0i0",[0,"首    页","","",-1,-1,0,"index.asp","_self","","","","",5,5,0,"","",0,0,0,1,1,"#000000",1,"#993333",1,"","",1,1,0,0,"#FFFFF7","#000000","#FFFFFF","#FFFFFF","bold 9pt Arial","bold 9pt Arial",0,0],71,44);
+stm_aix("p0i1","p0i0",[0,"学校概况","","",-1,-1,0,"xxgk.asp","_self","","","","",5,5,0,"","",-1,-1,0,1,1,"#000000",1,"#993333",1,"","",3],71,44);
+stm_bpx("p1","p0",[1,4,0,2,0,5,0,0,80,"stEffect(\"slip\")",-2,"stEffect(\"slip\")",-2,80,0,0,"#666666","#000000"]);
+stm_aix("p1i0","p0i0",[0,"学校简介","","",-1,-1,0,"xxgk.asp?classid=14","_self","","","","",0,0,0,"","",0,0,0,1,1,"#045B17",0,"#1F7700",0,"","",3,3,0,0,"#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","9pt Arial","9pt Arial"],71,15);
+stm_aix("p1i1","p1i0",[0,"学校领导","","",-1,-1,0,"xxgk.asp?classid=15"],71,15);
+stm_aix("p1i2","p1i0",[0,"荣誉传真","","",-1,-1,0,"xxgk.asp?classid=16"],71,15);
+stm_aix("p1i3","p1i0",[0,"校园掠影","","",-1,-1,0,"xxgk.asp?classid=17"],71,15);
+stm_aix("p1i4","p1i0",[0,"领导关怀","","",-1,-1,0,"xxgk.asp?classid=18"],71,15);
+stm_ep();
+stm_aix("p0i2","p0i0",[0,"校园动态","","",-1,-1,0,"xydt.asp","_self","","","","",5,5,0,"","",-1,-1],71,44);
+stm_bpx("p2","p1",[]);
+stm_aix("p2i0","p1i0",[0,"公告","","",-1,-1,0,"xydt.asp?classid=19"],71,15);
+stm_aix("p2i1","p1i0",[0,"校园新闻","","",-1,-1,0,"xydt.asp?classid=20"],71,15);
+stm_aix("p2i2","p1i0",[0,"活动掠影","","",-1,-1,0,"xydt.asp?classid=21"],71,15);
+stm_aix("p2i3","p1i0",[0,"视频剪辑","","",-1,-1,0,"xydt.asp?classid=22"],71,15);
+stm_aix("p2i4","p1i0",[0,"媒体宣传","","",-1,-1,0,"xydt.asp?classid=100"],71,15);
+stm_ep();
+stm_aix("p0i3","p0i2",[0,"年级组","","",-1,-1,0,"njz.asp"],71,44);
+stm_bpx("p3","p1",[]);
+stm_aix("p3i0","p1i0",[0,"七年级","","",-1,-1,0,"njz.asp?classid=23"],71,15);
+stm_aix("p3i1","p1i0",[0,"八年级","","",-1,-1,0,"njz.asp?classid=24"],71,15);
+stm_aix("p3i2","p1i0",[0,"九年级","","",-1,-1,0,"njz.asp?classid=25"],71,15);
+stm_aix("p3i3","p1i0",[0,"教辅后勤","","",-1,-1,0,"njz.asp?classid=101"],71,15);
+stm_ep();
+stm_aix("p0i4","p0i2",[0,"党群工作","","",-1,-1,0,"dqgz.asp"],71,44);
+stm_bpx("p4","p1",[]);
+stm_aix("p4i0","p1i0",[0,"    党总支","","",-1,-1,0,"dqgz.asp?classid=26","_self","","","","",0,0,0,"","",-1,-1],76,15);
+stm_bpx("p5","p1",[1,2]);
+stm_aix("p5i0","p1i0",[0,"党支部","","",-1,-1,0,"dqgz.asp?classid=26"],76,15);
+stm_aix("p5i1","p1i0",[0,"廉政文化","","",-1,-1,0,"dqgz.asp?classid=12"],76,15);
+stm_ep();
+stm_aix("p4i1","p1i0",[0,"工会","","",-1,-1,0,"dqgz.asp?classid=29"],76,15);
+stm_aix("p4i2","p4i0",[0,"    共青团","","",-1,-1,0,"dqgz.asp?classid=36"],76,15);
+stm_bpx("p6","p5",[]);
+stm_aix("p6i0","p1i0",[0,"教工团支部","","",-1,-1,0,"dqgz.asp?classid=30","_self","","","","",0,0,0,"","",0,0,0,2],76,15);
+stm_aix("p6i1","p6i0",[0,"学生团支部","","",-1,-1,0,"dqgz.asp?classid=36"],76,15);
+stm_ep();
+stm_aix("p4i3","p1i0",[0," 女工委","","",-1,-1,0,"dqgz.asp?classid=11"],76,15);
+stm_ep();
+stm_aix("p0i5","p0i2",[0,"学校部门","","",-1,-1,0,"xxbm.asp"],71,44);
+stm_bpx("p7","p1",[]);
+stm_aix("p7i0","p1i0",[0,"党政办公室","","",-1,-1,0,"xxbm.asp?classid=34"],71,15);
+stm_aix("p7i1","p1i0",[0,"政教处","","",-1,-1,0,"xxbm.asp?classid=27"],71,15);
+stm_aix("p7i2","p1i0",[0,"教务处","","",-1,-1,0,"xxbm.asp?classid=35"],71,15);
+stm_aix("p7i3","p1i0",[0,"总务处","","",-1,-1,0,"xxbm.asp?classid=37"],71,15);
+stm_ep();
+stm_aix("p0i6","p0i2",[0,"德育之窗","","",-1,-1,0,"dyzc.asp"],71,44);
+stm_bpx("p8","p1",[]);
+stm_aix("p8i0","p1i0",[0,"班主任队伍","","",-1,-1,0,"dyzc.asp?classid=38"],76,15);
+stm_aix("p8i1","p1i0",[0,"班级建设","","",-1,-1,0,"dyzc.asp?classid=28"],76,15);
+stm_aix("p8i2","p1i0",[0,"德育管理","","",-1,-1,0,"dyzc.asp?classid=13"],76,15);
+stm_aix("p8i3","p1i0",[0,"课外访万家","","",-1,-1,0,"dyzc.asp?classid=91"],76,15);
+stm_aix("p8i4","p1i0",[0,"心灵憩园","","",-1,-1,0,"dyzc.asp?classid=41"],76,15);
+stm_aix("p8i5","p1i0",[0,"健康教育","","",-1,-1,0,"dyzc.asp?classid=99"],76,15);
+stm_ep();
+stm_aix("p0i7","p0i2",[0,"教学教研","","",-1,-1,0,"jxjy.asp"],71,44);
+stm_bpx("p9","p1",[]);
+stm_aix("p9i0","p1i0",[0,"教学管理","","",-1,-1,0,"jxjy.asp?classid=33"],76,15);
+stm_aix("p9i1","p1i0",[0,"教研成果","","",-1,-1,0,"jxjy.asp?classid=42"],76,15);
+stm_aix("p9i2","p1i0",[0,"教研组建设","","",-1,-1,0,"jxjy.asp?classid=43"],76,15);
+stm_aix("p9i3","p1i0",[0,"中小学教\r\n育教学衔接","","",-1,-1,0,"jxjy.asp?classid=44"],76,30);
+stm_aix("p9i3","p1i0",[0,"课内比教学","","",-1,-1,0,"jxjy.asp?classid=92"],76,30);
+stm_aix("p9i4","p1i0",[0,"校本课程","","",-1,-1,0,"jxjy.asp?classid=31"],76,15);
+stm_ep();
+stm_aix("p0i8","p0i2",[0,"教师专栏","","",-1,-1,0,"jszl.asp"],71,44);
+stm_bpx("p10","p1",[]);
+stm_aix("p10i0","p4i0",[0,"  名师风采","","",-1,-1,0,"jszl.asp?classid=39"],76,15);
+stm_bpx("p11","p5",[]);
+stm_aix("p11i0","p6i0",[0,"省市优秀教师","","",-1,-1,0,"jszl.asp?classid=39"],76,15);
+stm_aix("p11i1","p6i0",[0,"校级骨干教师","","",-1,-1,0,"jszl.asp?classid=40"],76,15);
+stm_ep();
+stm_aix("p10i1","p1i0",[0,"教学经验","","",-1,-1,0,"jszl.asp?classid=46"],76,15);
+stm_ep();
+stm_aix("p0i9","p0i2",[0,"学生天地","","",-1,-1,0,"xstd.asp"],71,44);
+stm_bpx("p12","p1",[]);
+stm_aix("p12i0","p1i0",[0,"佳作欣赏","","",-1,-1,0,"xstd.asp?classid=49"],76,15);
+stm_aix("p12i1","p1i0",[0,"学习交流","","",-1,-1,0,"xstd.asp?classid=50"],76,15);
+stm_aix("p12i2","p1i0",[0,"学生风采","","",-1,-1,0,"xstd.asp?classid=51"],76,15);
+stm_aix("p12i3","p1i0",[0,"毕业留影","","",-1,-1,0,"xstd.asp?classid=52"],76,15);
+stm_ep();
+stm_aix("p0i10","p0i2",[0,"家校互动","","",-1,-1,0,"jxhd.asp"],71,44);
+stm_bpx("p13","p1",[]);
+stm_aix("p13i0","p1i0",[0,"家教锦囊","","",-1,-1,0,"jxhd.asp?classid=53"],76,15);
+stm_aix("p13i1","p1i0",[0,"家长心声","","",-1,-1,0,"jxhd.asp?classid=54"],76,15);
+stm_aix("p13i2","p1i0",[0,"家长学校","","",-1,-1,0,"jxhd.asp?classid=102"],76,15);
+stm_ep();
+stm_aix("p0i11","p0i2",[0,"资源中心","","",-1,-1,0,"zyzx.asp"],71,44);
+stm_bpx("p14","p1",[]);
+stm_aix("p14i0","p4i0",[0,"  图书阅览","","",-1,-1,0,"zyzx.asp?classid=48"],71,15);
+stm_bpx("p15","p5",[]);
+stm_aix("p15i0","p1i0",[0,"三峡大学\r\n  图书馆","","",-1,-1,0,"zyzx.asp?classid=48"],76,0);
+stm_aix("p15i1","p1i0",[0,"博看电子\r\n阅览","","",-1,-1,0,"zyzx.asp?classid=49"],76,0);
+stm_ep();
+stm_aix("p14i1","p1i0",[0,"课程资源","","",-1,-1,0,"zyzx.asp?classid=48"],71,15);
+stm_ep();
+stm_aix("p0i12","p0i2",[0,"校友风采","","",-1,-1,0,"xyfc.asp"],71,44);
+stm_bpx("p16","p1",[]);
+stm_aix("p16i0","p1i0",[0,"校友动态","","",-1,-1,0,"xyfc.asp?classid=57"],76,15);
+stm_ep();
+stm_aix("p0i13","p0i2",[0,"招生专栏","","",-1,-1,0,"zszl.asp"],71,44);
+stm_bpx("p17","p1",[]);
+stm_aix("p17i0","p1i0",[0,"招生问答","","",-1,-1,0,"zszl.asp?classid=58"],76,15);
+stm_aix("p17i1","p1i0",[0,"录取通知","","",-1,-1,0,"zszl.asp?classid=59"],76,15);
+stm_ep();
+stm_ep();
+stm_em();
+
+/*
+     FILE ARCHIVED ON 23:10:57 Apr 01, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 07:38:59 Mar 14, 2020.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  PetaboxLoader3.datanode: 202.738 (5)
+  exclusion.robots.policy: 0.314
+  exclusion.robots: 0.362
+  load_resource: 462.566
+  LoadShardBlock: 138.703 (3)
+  captures_list: 187.869
+  RedisCDXSource: 24.95
+  PetaboxLoader3.resolve: 384.303 (3)
+  esindex: 0.025
+  CDXLines.iter: 19.017 (3)
+*/
---
+++ 2/宜昌市第八中学（三峡大学附属中学）！_files/5198153.js
@@ -0,0 +1,31 @@
+document.write ('<a href="https://web.archive.org/web/20160401231740/http://www.51.la/?5198153" target="_blank"><img alt="&#x35;&#x31;&#x2E;&#x6C;&#x61;&#x20;&#x4E13;&#x4E1A;&#x3001;&#x514D;&#x8D39;&#x3001;&#x5F3A;&#x5065;&#x7684;&#x8BBF;&#x95EE;&#x7EDF;&#x8BA1;" src="https://web.archive.org/web/20160401231740/http://icon.ajiang.net/icon_12.gif" style="border:none" /></a>\n');
+var a8153tf="51la";var a8153pu="";var a8153pf="51la";var a8153su=window.location;var a8153sf=document.referrer;var a8153of="";var a8153op="";var a8153ops=1;var a8153ot=1;var a8153d=new Date();var a8153color="";if (navigator.appName=="Netscape"){a8153color=screen.pixelDepth;} else {a8153color=screen.colorDepth;}
+try{a8153tf=top.document.referrer;}catch(e){}
+try{a8153pu =window.parent.location;}catch(e){}
+try{a8153pf=window.parent.document.referrer;}catch(e){}
+try{a8153ops=document.cookie.match(new RegExp("(^| )AJSTAT_ok_pages=([^;]*)(;|$)"));a8153ops=(a8153ops==null)?1: (parseInt(unescape((a8153ops)[2]))+1);var a8153oe =new Date();a8153oe.setTime(a8153oe.getTime()+60*60*1000);document.cookie="AJSTAT_ok_pages="+a8153ops+ ";path=/;expires="+a8153oe.toGMTString();a8153ot=document.cookie.match(new RegExp("(^| )AJSTAT_ok_times=([^;]*)(;|$)"));if(a8153ot==null){a8153ot=1;}else{a8153ot=parseInt(unescape((a8153ot)[2])); a8153ot=(a8153ops==1)?(a8153ot+1):(a8153ot);}a8153oe.setTime(a8153oe.getTime()+365*24*60*60*1000);document.cookie="AJSTAT_ok_times="+a8153ot+";path=/;expires="+a8153oe.toGMTString();}catch(e){}
+try{if(document.cookie==""){a8153ops=-1;a8153ot=-1;}}catch(e){}
+a8153of=a8153sf;if(a8153pf!=="51la"){a8153of=a8153pf;}if(a8153tf!=="51la"){a8153of=a8153tf;}a8153op=a8153pu;try{lainframe}catch(e){a8153op=a8153su;}
+a8153src='https://web.archive.org/web/20160401231740/http://web.51.la:82/go.asp?svid=16&id=5198153&tpages='+a8153ops+'&ttimes='+a8153ot+'&tzone='+(0-a8153d.getTimezoneOffset()/60)+'&tcolor='+a8153color+'&sSize='+screen.width+','+screen.height+'&referrer='+escape(a8153of)+'&vpage='+escape(a8153op)+'&vvtime='+a8153d.getTime();
+setTimeout('a8153img = new Image;a8153img.src=a8153src;',0);
+/*
+     FILE ARCHIVED ON 23:17:40 Apr 01, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 07:56:10 Mar 14, 2020.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 94.041 (3)
+  load_resource: 168.776
+  PetaboxLoader3.datanode: 95.993 (5)
+  exclusion.robots.policy: 0.176
+  CDXLines.iter: 12.785 (3)
+  RedisCDXSource: 6.214
+  exclusion.robots: 0.191
+  PetaboxLoader3.resolve: 81.705 (2)
+  esindex: 0.014
+  captures_list: 117.37
+*/
